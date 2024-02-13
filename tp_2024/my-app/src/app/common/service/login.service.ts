9a48400074be3835268e416749d184fc0c460468
DidierDefrance <didier@d-defrance.fr>
2024-02-13 17:20:19 +0100

    s

--- tp_2024/my-app/src/app/common/service/login.service.ts
+++ tp_2024/my-app/src/app/common/service/login.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Devise } from '../data/devise';
+import { Login, LoginResponse } from '../data/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  baseUrl = "https://www.d-defrance.fr/tp/standalone-login-api/public";
+
+  constructor(private http: HttpClient){
+  }
+ 
+   public postLogin$(loginRequest : Login) : Observable<LoginResponse>{
+      let url = this.baseUrl + "/auth";
+      return this.http.post<LoginResponse>(url,loginRequest);
+   }
 }
--- tp_2024/my-app/src/app/login/login.component.ts
+++ tp_2024/my-app/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Login } from '../common/data/login';
+import { Login, LoginResponse } from '../common/data/login';
+import { LoginService } from '../common/service/login.service';
+import { messageFromError } from '../common/util/util';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,30 @@ import { Login } from '../common/data/login';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-   login = new Login();
+   //login = new Login();
+   login = new Login("user1","pwd1","user");
    message = "";
+   status = false;
+
+   constructor(private loginService: LoginService){
+      //injection de dépendance
+   }
+
    onLogin(){
     //V1:
-    this.message="valeurs saisies=" + JSON.stringify(this.login);
+    //this.message="valeurs saisies=" + JSON.stringify(this.login);
+    //V2:
+    this.loginService.postLogin$(this.login).subscribe(
+      {
+        next: (loginResponse: LoginResponse) => {
+          this.message=loginResponse.message;
+          this.status = loginResponse.status;
+        },
+        error: (err) => {
+          console.log("error:" + err);
+          this.message = messageFromError(err, "echec login")
+        }
+      }
+    );
    }
 }
