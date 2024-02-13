a00ab62e448519a8b4ad7fcb2e10fdada977d23b
DidierDefrance <didier@d-defrance.fr>
2024-02-13 17:07:31 +0100

    s

--- tp_2024/my-app/src/app/common/data/login.ts
+++ tp_2024/my-app/src/app/common/data/login.ts
@@ -4,3 +4,11 @@ export class Login {
         public  password : string ="",
         public  roles : string ="" ){}
 }
+
+export class LoginResponse {
+    constructor(
+        public  username : string ="",
+        public  message : string ="",
+        public  status : boolean =false,
+        public  token : string ="" ){}
+}
